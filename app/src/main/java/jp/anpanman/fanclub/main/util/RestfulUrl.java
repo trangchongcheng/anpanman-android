8a06842c20ccd25f9b223c6c93c3590293a5cbf7
Phatvt <admin@admins-Mac-mini-5.local>
2016-08-30 17:23:11 +0700

    Modified URL constants ( appropriate for request GET method ) Remove "/" end of LINE after every request links

--- app/src/main/java/jp/anpanman/fanclub/main/util/RestfulUrl.java
+++ app/src/main/java/jp/anpanman/fanclub/main/util/RestfulUrl.java
@@ -10,21 +10,21 @@ public class RestfulUrl {
     public final static String NOTIFY_HOST_GET_USER_INFO = "https://mb.api.cloud.nifty.com/2013-09-01/classes/User/";
 
     //== web view urls
-    public final static String WEB_VIEW_DOMAIN_PRODUCT_MODE = "http://fcapp.anpanman.jp/";
-    public final static String WEB_VIEW_DOMAIN_STAGING_MODE = "http://stg-fcapp.anpanman.jp/";
+    public final static String WEB_VIEW_DOMAIN_PRODUCT_MODE = "http://fcapp.anpanman.jp";
+    public final static String WEB_VIEW_DOMAIN_STAGING_MODE = "http://stg-fcapp.anpanman.jp";
     public final static String WEB_VIEW_DOMAIN = WEB_VIEW_DOMAIN_STAGING_MODE;
 
-    public final static String URL_NEWS = WEB_VIEW_DOMAIN+ "new/?id=";
+    public final static String URL_NEWS = WEB_VIEW_DOMAIN+ "/new?id=";
     public final static String URL_PORTAL_SITE = WEB_VIEW_DOMAIN+ "?id=";
-    public final static String URL_PRESENTS = WEB_VIEW_DOMAIN+ "present/?id=";
-    public final static String URL_COUPON = WEB_VIEW_DOMAIN+ "otoku/?id=";
-    public final static String URL_ACCOUNT_SETTING = WEB_VIEW_DOMAIN+ "account/?id=";
-    public final static String URL_TERMS = WEB_VIEW_DOMAIN+ "terms/?id=";
-    public final static String URL_POLICY = WEB_VIEW_DOMAIN+ "privacy_policy/?id=";
-    public final static String URL_CONTACT = WEB_VIEW_DOMAIN+ "contact/?id=";
-    public final static String URL_GURIDINGU = WEB_VIEW_DOMAIN+ "greeting/?id=";
-    public final static String URL_WALL = WEB_VIEW_DOMAIN+ "wallpaper/?id=";
-    public final static String URL_REGISTER_MYPAGE = WEB_VIEW_DOMAIN+ "account/F-4-1.html?id=";
+    public final static String URL_PRESENTS = WEB_VIEW_DOMAIN+ "/present?id=";
+    public final static String URL_COUPON = WEB_VIEW_DOMAIN+ "/otoku?id=";
+    public final static String URL_ACCOUNT_SETTING = WEB_VIEW_DOMAIN+ "/account?id=";
+    public final static String URL_TERMS = WEB_VIEW_DOMAIN+ "/terms?id=";
+    public final static String URL_POLICY = WEB_VIEW_DOMAIN+ "/privacy_policy?id=";
+    public final static String URL_CONTACT = WEB_VIEW_DOMAIN+ "/contact?id=";
+    public final static String URL_GURIDINGU = WEB_VIEW_DOMAIN+ "/greeting?id=";
+    public final static String URL_WALL = WEB_VIEW_DOMAIN+ "/wallpaper?id=";
+    public final static String URL_REGISTER_MYPAGE = WEB_VIEW_DOMAIN+ "/account/F-4-1.html?id=";
 
-    public final static String API_UPDATED_TIME =  WEB_VIEW_DOMAIN + "json/setting.json";
+    public final static String API_UPDATED_TIME =  WEB_VIEW_DOMAIN + "/json/setting.json";
 }
